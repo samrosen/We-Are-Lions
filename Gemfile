340b7613cd768ae41524a7d9e24d3c2e1351ca2d
Dan Hodos <danhodos@gmail.com>
2011-05-16 15:10:11 -0500

    Deployment files. Weeeeeeeelaaaaah

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "http://rubygems.org"
+
+gem "one_design_deployment", :git => "git@github.com:onedesign/one_design_deployment.git"
---
+++ config/deploy.rb
@@ -0,0 +1,6 @@
+require 'bundler/setup'
+require 'one_design/static'
+
+set :application, "wearelions.org"
+set :host_name, "wearelions.org"
+set :repository,  "git@github.com:samrosen/We-Are-Lions.git"
